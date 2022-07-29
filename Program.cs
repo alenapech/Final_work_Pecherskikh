f15c109eefe3b4c335d152404182e8f4cca0f723
Pavel Pecherskih <pecherskih@netcracker.com>
2022-07-29 23:14:47 +0400

    method of fill array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] getInputArray() 
+{
+    string[] array = {"Monday", "Day", ":)", "Baby", "Sun", "Lesson", "Learning", "Book", "Cat", "Dog"};
+    return array;
+}
+
