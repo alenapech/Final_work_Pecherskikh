5c6398392de12a877e5faca7c59f47d9bd859c99
Pavel Pecherskih <pecherskih@netcracker.com>
2022-07-29 23:16:15 +0400

    method of creating new array with strings of 3 symbols

--- Program.cs
+++ Program.cs
@@ -14,4 +14,19 @@ void printArray(string[] currentArray)
 Console.WriteLine();
 }
 
+string[] getOutputArray(string[] currentArray)
+{
+    string[] outputArray = new string[currentArray.Length];
+    int k = 0;
+
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if(currentArray[i].Length <= 3) 
+        {
+            outputArray[k] = currentArray[i];
+            k++;
+        }
+    }
+    return outputArray; 
+}
 
